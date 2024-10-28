cf7a0f0a5fbfdfee948d1a5bb2c2febfbc5e31d9
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-28 11:14:28 +0100

    add action possibility

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,7 +12,7 @@ export function Card(props: CardProps) {
         <img src={props.imageSrc} alt={props.title} className="card__image" />
         <h3 className="card__title">{props.title}</h3>
         <p className="card__description">{props.description}</p>
-        <div className="card__actions"></div>
+        {props.actions && <div className="card__actions">{props.actions}</div> }
       </article>
     </>
   );
