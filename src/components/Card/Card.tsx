46d62035501773129b91f7306cfca5476bd63067
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-28 10:57:17 +0100

    add basic html to card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,15 @@
+
+
+
+export function Card() {
+  return (
+   <>
+   <article className="card">
+   <img src="" alt="" className="card__image"/>
+   <h3 className="card__title"></h3>
+   <p className="card__description"></p>
+   <div className="card__actions"></div>
+   </article>
+   </>
+  )
+}
